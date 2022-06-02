773bf9e2187d9049b1b27f12632c28bea2cc6282
Tirth Patel <tpatel95@myseneca.ca>
2022-06-02 03:39:11 +0000

    Added Terraform Code to DEV branch

---
+++ Assignment1-Terraform-code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Tirth"
+    "App"   = "Containerized Application"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Assignment-1"
+}
