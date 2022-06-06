3f4e2b59681992ee2dd940a0558d25ca5dd41628
EC2 Default User <ec2-user@ip-172-31-39-186.ec2.internal>
2022-06-06 18:54:15 +0000

    adding tf files

---
+++ terraform/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Neethu"
+    "App"   = "catsndogs"
+    "Project" = "Assignment1"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Assignment1"
+}
