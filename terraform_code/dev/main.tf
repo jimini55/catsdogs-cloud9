40dc1f2a715d3d1732284c8ec8df1146b0762b75
romiya11 <romiyamahat220@gmail.com>
2022-06-07 03:20:53 +0000

    terraform code commit

---
+++ terraform_code/dev/main.tf
@@ -0,0 +1,105 @@
+
+
+#  Define the provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source for AMI id
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Data source for availability zones in us-east-1
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# Data block to retrieve the default VPC id
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_instance" "my_ami" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  key_name               = aws_key_pair.romiya_key.key_name
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  tags = {
+    Name = "romiya-instance"
+  }
+}
+
+
+
+
+# Adding SSH key to Amazon EC2
+resource "aws_key_pair" "romiya_key" {
+  key_name   = "myassignment-dev"
+  public_key = file("myassignment-dev.pub")
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH from everywhere"
+    from_port        = 8081
+    to_port          = 8081
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ass_romiya-sg"
+  }
+}
+
+
+resource "aws_ecr_repository" "romiya-assignment1" {
+  name                 = "romiya-assignment1"
+  image_tag_mutability = "MUTABLE"
+}
