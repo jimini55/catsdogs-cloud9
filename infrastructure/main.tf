bc8235e5c63f2de863d09b0ab3750830481585b5
Fahim Ahmed <fahmed122@myseneca.ca>
2022-06-08 21:02:14 +0000

    Added ECR to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -123,3 +123,13 @@ resource "aws_eip" "static_eip" {
     }
   )
 }
+
+# ECR Repo
+resource "aws_ecr_repository" "Asg1ECR" {
+  name                 = "docker"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
