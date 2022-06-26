9718b98ba1281bcc21325ba7f8eae5fdbe808695
EC2 Default User <ec2-user@ip-172-31-15-54.ec2.internal>
2022-06-26 03:00:42 +0000

    created workflows and repository for cat and dogs

--- terraform_code/dev/instances/config.tf
+++ terraform_code/dev/instances/config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kajalsodhi-assignment1" // Bucket where to SAVE Terraform State
+    bucket = "kajalsodhi-assignment2" // Bucket where to SAVE Terraform State
     key    = "dev/terraform.tfstate"  // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"              // Region where bucket is created
   }
